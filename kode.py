40f47787c9b4a6763a794d3ad4f189c6fdab68d3
Kasper <kasper@Kaspers-MacBook-Pro.local>
2021-01-29 11:09:38 +0100

    Create kode.py

---
+++ kode.py
@@ -0,0 +1,85 @@
+#HTTP Server template
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST_NAME = '127.0.0.1' #locathost - http://127.0.0.1
+PORT_NUMBER = 1234 # Maybe set this to 1234 / So, complete address would be: http://127.0.0.1:1234
+# Web servers example: http://www.ntnu.edu:80
+
+# Handler of HTTP requests / responses
+class MyHandler(BaseHTTPRequestHandler):
+	def do_HEAD(s):
+		s.send_response(200)
+		s.send_header("Content-type", "text/html")
+		s.end_headers()
+	def do_GET(s):
+		"""Respond to a GET request."""
+		s.send_response(200)
+		s.send_header("Content-type", "text/html")
+		s.end_headers()
+		
+		# Check what is the path
+		path = s.path
+		if path.find("/") != -1 and len(path) == 1:
+			s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
+			s.wfile.write(bytes("<body><p>Current path: " + path + "</p>", "utf-8"))
+			s.wfile.write(bytes('</body></html>', "utf-8"))
+		elif path.find("/info") != -1:
+			s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
+			s.wfile.write(bytes("<body><p>Current path: " + path + "</p>", "utf-8"))
+			s.wfile.write(bytes("<body><p>Let's order a chair</p>", "utf-8"))
+			s.wfile.write(bytes('</body></html>', "utf-8"))
+		elif path.find("/setLength") != -1:
+			s.wfile.write(bytes('<html><body><h2>Chair</h2>', 'utf-8'))
+			s.wfile.write(bytes('<form action="/setLength" method="post">', 'utf-8'))
+			s.wfile.write(bytes('<label for="clength">Set Length:</label><br>', 'utf-8'))
+			s.wfile.write(bytes('<input type="text" id="clength" name="clength" value="100"><br><br>', 'utf-8'))
+            s.wfile.write(bytes('<label for="clength">Set Length:</label><br>', 'utf-8'))
+			s.wfile.write(bytes('<input type="text" id="clength" name="clength" value="100"><br><br>', 'utf-8'))
+			s.wfile.write(bytes('<input type="submit" value="Submit">', 'utf-8'))
+			s.wfile.write(bytes('</form></body></html>', 'utf-8'))
+		else:
+			s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
+			s.wfile.write(bytes("<body><p>The path: " + path + "</p>", "utf-8"))
+			s.wfile.write(bytes('</body></html>', "utf-8"))
+			
+			
+	def do_POST(s):
+
+		s.send_response(200)
+		s.send_header("Content-type", "text/html")
+		s.end_headers()
+		
+		# Check what is the path
+		path = s.path
+		print("Path: ", path)
+		if path.find("/setLength") != -1:
+			content_len = int(s.headers.get('Content-Length'))
+			post_body = s.rfile.read(content_len)
+			param_line = post_body.decode()
+			print("Body: ", param_line)
+			
+			s.wfile.write(bytes('<html><body><h2>Chair</h2>', 'utf-8'))
+			s.wfile.write(bytes('<form action="/setLength" method="post">', 'utf-8'))
+			s.wfile.write(bytes('<label for="clength">Set Length:</label><br>', 'utf-8'))
+			s.wfile.write(bytes('<input type="text" id="clength" name="clength" value="100"><br><br>', 'utf-8'))
+			s.wfile.write(bytes('<input type="submit" value="Submit">', 'utf-8'))
+			
+			s.wfile.write(bytes('<p>' + param_line + '</p>', 'utf-8'))
+			
+			s.wfile.write(bytes('</form></body></html>', 'utf-8'))
+			
+			
+ 
+if __name__ == '__main__':
+	server_class = HTTPServer
+	httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+	
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
+
+
+
