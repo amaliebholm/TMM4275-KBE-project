a7d882debe99d213a600b7fd1f7354af21a82f43
Amalie Berge Holm <amalie.berge.holm@live.no>
2021-02-19 13:19:37 +0100

    fuseki

--- fuseki_test.py
+++ fuseki_test.py
@@ -4,7 +4,7 @@ import requests
 URL = "http://127.0.0.1:3030/kbe/query"
   
 # defining a query params 
-PARAMS = {'query':'PREFIX kbe:<http://www.my-kbe.com/shapes.owl#>SELECT ?side WHERE {?cube a kbe:Cube.?cube kbe:hasSide ?side.}'} 
+PARAMS = {'query':'PREFIX kbe:<http://www.kbe_chair.com/.owl#>SELECT ?var WHERE {?apron a kbe:Apron.?Apron kbe:apronMax ?var.}'} 
         # ?subject = ? what we would like to retrieve (cube a kbe:)
             #kbe: = 'PREFIX kbe:<http://www.my-kbe.com/shapes.owl#>'
         # ?predicate (cuve kbe:hasSide)
